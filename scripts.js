76c3919f6c87a5b12ee130714bdfc19c862d841f
zimores321 <59705068+zimores321@users.noreply.github.com>
2021-04-24 16:36:49 -0400

    1

--- scripts.js
+++ scripts.js
@@ -6,12 +6,30 @@ $('document').ready(function(){
 			var regionName = data.regionName;
 			var regionDisc = data.regionDisc;
 			$.each( data.cities, function( key, val ) {
-				$("#cities").append( "<a href='" + val.link + "' id='" + key + "'>" + val.name + "</a>" );
+				$("#cities").append( "<a class='cityNames' href='" + val.link + "' id='" + key + "'>" + val.name + "</a>" );
 				
 			});
 
 		});
 		
 	});
+
+
+	$(".cityNames").click(function(e){
+		e.preventDefault();
+
+		$("#poi").empty();
+		$.getJSON( this.href, function( data ) {
+			var cities = [];
+			var regionName = data.cityName;
+			var regionDisc = data.cityDiscription;
+			$.each( data.pointsOfInterest, function( key, val ) {
+				$("#poi").append( "<a class='pointsOfInterest' id='" + key + "'>" + val.name + "</a>" );
+				
+			});
+
+		});
+
+	})
 	
 })
