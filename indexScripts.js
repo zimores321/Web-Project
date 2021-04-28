c20c4b20a7ee72ebdd619148d5c1a91284eaabad
zimores321 <59705068+zimores321@users.noreply.github.com>
2021-04-28 18:02:37 -0400

    1

--- indexScripts.js
+++ indexScripts.js
@@ -13,7 +13,23 @@ $('document').ready(function(){
 				case "npc":
 					window.location.href = window.location.href + gameSource + '/NPCs/NPC.html' ;
 			}
-		}	
-
-	
+		}		
+})
+$(document).on('click','.cityNames',function()
+			   {
+			function goToSource(page) {
+			var gameSource = Game.value;
+			switch(page) {
+				case "utilities":
+					window.location.href = window.location.href + gameSource + '/Utilities/utilities.html';
+				case "location":
+					window.location.href = window.location.href + gameSource + '/Locations/locations.html' ;
+				case "player":
+					window.location.href = window.location.href + gameSource + '/Players/players.html' ;
+				case "note":
+					window.location.href = window.location.href + gameSource + '/Notes/notes.html' ;
+				case "npc":
+					window.location.href = window.location.href + gameSource + '/NPCs/NPC.html' ;
+			}
+		}		
 })
