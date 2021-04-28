c736794f048c19fa38af25a5d3eeeef04646c217
zimores321 <59705068+zimores321@users.noreply.github.com>
2021-04-28 19:10:35 -0400

    1

--- indexScripts.js
+++ indexScripts.js
@@ -1,19 +1,21 @@
 
 $('document').ready(function(){
-	
+	$(select.Game).change(function(){
+		let selectedItem = $(this).children("option:selected").val();
+        alert("You have selected the name - " + selectedItem);		
+	})
 })	
 function goToSource(page) {
-			var gameSource = Game.value;
 			switch(page) {
 				case "utilities":
-					window.location.href = window.location.href + gameSource + '/Utilities/utilities.html';
+					window.location.href = window.location.href + selectedItem + '/Utilities/utilities.html';
 				case "location":
-					window.location.href = window.location.href + gameSource + '/Locations/locations.html' ;
+					window.location.href = window.location.href + selectedItem + '/Locations/locations.html' ;
 				case "player":
-					window.location.href = window.location.href + gameSource + '/Players/players.html' ;
+					window.location.href = window.location.href + selectedItem + '/Players/players.html' ;
 				case "note":
-					window.location.href = window.location.href + gameSource + '/Notes/notes.html' ;
+					window.location.href = window.location.href + selectedItem + '/Notes/notes.html' ;
 				case "npc":
-					window.location.href = window.location.href + gameSource + '/NPCs/NPC.html' ;
+					window.location.href = window.location.href + selectedItem + '/NPCs/NPC.html' ;
 			}
 		}	
