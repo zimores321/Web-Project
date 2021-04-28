e7b5200257045fee570ac9f663e8a88eb61f8719
zimores321 <59705068+zimores321@users.noreply.github.com>
2021-04-28 18:43:30 -0400

    1

--- indexScripts.js
+++ indexScripts.js
@@ -1,22 +1,8 @@
 
 $('document').ready(function(){
-	function goToSource(page) {
-			var gameSource = Game.value;
-			switch(page) {
-				case "utilities":
-					window.location.href = window.location.href + gameSource + '/Utilities/utilities.html';
-				case "location":
-					window.location.href = window.location.href + gameSource + '/Locations/locations.html' ;
-				case "player":
-					window.location.href = window.location.href + gameSource + '/Players/players.html' ;
-				case "note":
-					window.location.href = window.location.href + gameSource + '/Notes/notes.html' ;
-				case "npc":
-					window.location.href = window.location.href + gameSource + '/NPCs/NPC.html' ;
-			}
-		}	
+	
 })	
-	function goToSource(page) {
+function goToSource(page) {
 			var gameSource = Game.value;
 			switch(page) {
 				case "utilities":
