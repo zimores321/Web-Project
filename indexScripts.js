c08b6f20a14eb30cd7e88e732fbc2be52f3162d5
zimores321 <59705068+zimores321@users.noreply.github.com>
2021-04-28 20:08:46 -0400

    1

--- indexScripts.js
+++ indexScripts.js
@@ -1,22 +1,23 @@
 
 $('document').ready(function(){
-	function goToSource(page) {
 	var gameSource = Game.value;
-	switch(page) {
-		case "utilities":
-			window.location.href = window.location.href + gameSource + '/Utilities/utilities.html';
-			break;
-		case "location":
-			window.location.href = window.location.href + gameSource + '/Locations/locations.html' ;
-			break;
-		case "player":
-			window.location.href = window.location.href + gameSource + '/Players/players.html' ;
-			break;
-		case "note":
-			window.location.href = window.location.href + gameSource + '/Notes/notes.html' ;
-			break;
-		case "npc":
-			window.location.href = window.location.href + gameSource + '/NPCs/NPC.html' ;
-	}
-}	
 })	
+$(document).on('click','.Loc',function(){
+	window.location.href = window.location.href + gameSource + '/Locations/locations.html' ;
+})
+
+$(document).on('click','.Note',function(){
+	window.location.href = window.location.href + gameSource + '/Notes/notes.html' ;
+})
+
+$(document).on('click','.Player',function(){
+	window.location.href = window.location.href + gameSource + '/Players/players.html' ;
+})
+
+$(document).on('click','.NPC',function(){
+	window.location.href = window.location.href + gameSource + '/NPCs/NPC.html' ;
+})
+
+$(document).on('click','.Uti',function(){
+	window.location.href = window.location.href + gameSource + '/Utilities/utilities.html';
+})
